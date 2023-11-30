c77c4d342d4d1bdf5e4aeeca83199bb14b293d70
Zulfequar Ali <z.ali@sundewsolutions.com>
2023-11-30 11:23:09 +0530

    Add more badge examples Add badges to demonstrate different types of animations.

--- lib/main.dart
+++ lib/main.dart
@@ -38,17 +38,177 @@ class _HomePageState extends State<HomePage> {
         foregroundColor: Colors.white,
         title: const Text('Badges Demo'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            badges.Badge(
+            const badges.Badge(
               badgeContent: Text('5'),
               child: Icon(
-                Icons.shopping_bag,
+                Icons.shopping_cart,
                 color: Colors.indigo,
                 size: 50.0,
               ),
+            ),
+            const SizedBox(height: 20),
+            Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: [
+                badges.Badge(
+                  badgeContent: const Text('5'),
+                  position: badges.BadgePosition.topEnd(top: -2, end: -2),
+                  badgeAnimation: const badges.BadgeAnimation.rotation(
+                    animationDuration: Duration(seconds: 1),
+                    loopAnimation: true,
+                  ),
+                  badgeStyle: badges.BadgeStyle(
+                    shape: badges.BadgeShape.square,
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide:
+                        const BorderSide(color: Colors.indigo, width: 2),
+                    borderGradient: const badges.BadgeGradient.linear(
+                        colors: [Colors.white, Colors.blue, Colors.red]),
+                    badgeGradient: const badges.BadgeGradient.linear(
+                        colors: [Colors.red, Colors.greenAccent]),
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                  ),
+                  child: const Icon(
+                    Icons.shopping_bag,
+                    color: Colors.black,
+                    size: 50.0,
+                  ),
+                ),
+                badges.Badge(
+                  badgeContent: const Text(
+                    '20',
+                    style: TextStyle(fontSize: 10),
+                  ),
+                  position: badges.BadgePosition.topEnd(top: -2, end: -2),
+                  badgeAnimation: const badges.BadgeAnimation.slide(
+                    animationDuration: Duration(seconds: 1),
+                    loopAnimation: true,
+                  ),
+                  badgeStyle: const badges.BadgeStyle(
+                    badgeColor: Colors.yellow,
+                  ),
+                  child: const Icon(
+                    Icons.mail,
+                    color: Colors.green,
+                    size: 50.0,
+                  ),
+                ),
+                badges.Badge(
+                  badgeContent: const Text(
+                    '3',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  ),
+                  position: badges.BadgePosition.topEnd(top: -10, end: 0),
+                  badgeAnimation: const badges.BadgeAnimation.fade(
+                    animationDuration: Duration(seconds: 2),
+                    disappearanceFadeAnimationDuration: Duration(seconds: 1),
+                    loopAnimation: true,
+                  ),
+                  badgeStyle: const badges.BadgeStyle(
+                    badgeColor: Colors.red,
+                  ),
+                  child: const Icon(
+                    Icons.alarm,
+                    color: Colors.blue,
+                    size: 50.0,
+                  ),
+                ),
+                badges.Badge(
+                  badgeContent: const Text(
+                    '7',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  ),
+                  position: badges.BadgePosition.topEnd(top: -5, end: 2),
+                  badgeAnimation: const badges.BadgeAnimation.scale(
+                    animationDuration: Duration(seconds: 1),
+                    loopAnimation: true,
+                  ),
+                  badgeStyle: const badges.BadgeStyle(
+                    badgeColor: Colors.indigo,
+                  ),
+                  child: const Icon(
+                    Icons.flight,
+                    color: Colors.amber,
+                    size: 50.0,
+                  ),
+                ),
+                badges.Badge(
+                  badgeContent: const Text(
+                    '4',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.white,
+                    ),
+                  ),
+                  position: badges.BadgePosition.bottomEnd (bottom: 10, end: 0),
+                  badgeAnimation: const badges.BadgeAnimation.size(
+                    animationDuration: Duration(seconds: 1),
+                    loopAnimation: true,
+                  ),
+                  badgeStyle: const badges.BadgeStyle(
+                    badgeColor: Colors.teal,
+                  ),
+                  child: const Icon(
+                    Icons.call,
+                    color: Colors.indigo,
+                    size: 50.0,
+                  ),
+                ),
+                badges.Badge(
+                  badgeContent: const Icon(Icons.check, size: 15, color: Colors.white,),
+                  position: badges.BadgePosition.topEnd (top: -10, end: 0),
+                  badgeAnimation: const badges.BadgeAnimation.scale(
+                    animationDuration: Duration(seconds: 2),
+                    loopAnimation: true,
+                  ),
+                  badgeStyle: const badges.BadgeStyle(
+                    badgeColor: Colors.pink,
+                    shape: badges.BadgeShape.twitter,
+                  ),
+                  child: const Icon(
+                    Icons.account_circle,
+                    color: Colors.blue,
+                    size: 50.0,
+                  ),
+                ),
+                badges.Badge(
+                  badgeContent: const Icon(Icons.check, size: 15, color: Colors.white,),
+                  position: badges.BadgePosition.topEnd (top: -10, end: 0),
+                  badgeStyle: const badges.BadgeStyle(
+                    badgeColor: Colors.blue,
+                    shape: badges.BadgeShape.instagram,
+                  ),
+                  child: const Icon(
+                    Icons.account_circle,
+                    color: Colors.lightBlueAccent,
+                    size: 50.0,
+                  ),
+                ),
+                badges.Badge(
+                  badgeContent: const Icon(Icons.check, size: 15, color: Colors.white,),
+                  position: badges.BadgePosition.topEnd (top: -10, end: 0),
+                  badgeStyle: const badges.BadgeStyle(
+                    badgeColor: Colors.blue,
+                    shape: badges.BadgeShape.twitter,
+                  ),
+                  child: const Icon(
+                    Icons.account_circle,
+                    color: Colors.pink,
+                    size: 50.0,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
